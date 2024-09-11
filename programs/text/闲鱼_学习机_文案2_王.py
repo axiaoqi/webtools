dd6931321214d8a48d83f112b40f114568eaf946
XiaoQi <1091874347@qq.com>
2024-09-12 00:42:19 +0800

    update

--- programs/text/闲鱼_学习机_文案2_王.py
+++ programs/text/闲鱼_学习机_文案2_王.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 try:
     from programs.text.xianyu_text import XianyuTextGenerator, XianyuImgTxt, generate_price
@@ -9,6 +10,44 @@ except ModuleNotFoundError:
     from programs.text.xianyu_text import XianyuTextGenerator, XianyuImgTxt, generate_price
 
 from programs.settings import SynologyDrive
+
+
+class XianyuTextGenerator2(XianyuTextGenerator):
+    # def __init__(self, data_dir: Path):
+    #     super().__init__(data_dir)
+    #     self.choice_items = []  # 选择出来的文案输出的结果
+
+    def run(self, start=None, end=None):
+
+        # 第一句，卖点
+        first_list = self._open_file(self.data_files_path[0])
+        choice_item = random.choice(first_list)
+        self.choice_items.append(choice_item)
+
+        # 第二句，标题
+        second_list = self._open_file(self.data_files_path[1])
+        choice_item = random.choice(second_list)
+        self.choice_items.append(choice_item)
+
+        # 第三句，故事，售卖原因
+        third_list = self._open_file(self.data_files_path[2])
+        choice_item = random.choice(third_list)
+        self.choice_items.append(choice_item)
+
+        # 第4句，配置，分割符分割
+        fourth_list = self._open_file_by_sep(self.data_files_path[3], sep='|||')
+        choice_item = random.choice(fourth_list)
+        self.choice_items.append(choice_item)
+
+        # 第5句，售后，分割符分割
+        fifth_list = self._open_file_by_sep(self.data_files_path[4], sep='|||')
+        choice_item = random.choice(fifth_list)
+        self.choice_items.append(choice_item)
+
+        return self.choice_items
+
+    def __str__(self):
+        return '\n'.join(['\n' + element if '【' in element else element for element in self.choice_items])
 
 
 def run():
@@ -41,12 +80,9 @@ def run():
     """
     咸鱼AI学习机
     """
-    data_dir = SynologyDrive / r'01新项目记录\闲鱼项目\AI学习机\01文案\文案素材1'
-    xianyu = XianyuTextGenerator(data_dir)
-
-    xianyu.run()  # start=4, end=7
-    print(xianyu)
-    return xianyu
+    x = XianyuTextGenerator2(SynologyDrive / r'01新项目记录\闲鱼项目\AI学习机\01文案\文案素材2_抄王')
+    x.run()
+    print(x)
 
 
 if __name__ == '__main__':
