d34ca7c03c200e2bfe26d365056c6fdbf83c8626
XiaoQi <1091874347@qq.com>
2024-10-13 23:28:58 +0800

    update

--- programs/查找pan_tpym_cn免费资料.py
+++ programs/查找pan_tpym_cn免费资料.py
@@ -4,6 +4,7 @@ import time
 import requests
 from lxml import etree
 from tqdm.auto import tqdm
+from 下载pan_tpym_cn文件3_升级版 import get_file_and_dir_url
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
@@ -59,9 +60,15 @@ def run(url_file, retries=10):
             file.write(element + '\n')
 
 
+# def check_url_available(url):
+#     dir_url_list, file_data_list, app_value, dir_id_value = get_file_and_dir_url(url)
+#     if len(file_data_list) > 0:
+#         dir_url_list_1, _file_data_list_1, app_value_1, dir_id_value_1 = get_file_and_dir_url(dir_url_list[0])
+
+
 if __name__ == '__main__':
-    url_file = r'C:\Users\e5\Desktop\url.txt'
-    # url_file = input('输入url列表文件路径：') or r'C:\Users\e5\Desktop\url.txt'
+    url_file = r'C:\Users\e5\Desktop\url.md'
+    # url_file = input('输入url列表文件路径：') or r'C:\Users\e5\Desktop\url.md'
     run(url_file=url_file)
 
     txt_url_list = []
@@ -77,3 +84,5 @@ if __name__ == '__main__':
     a = list(dict.fromkeys(txt_url_list))
     print(a)
     print(len(list(dict.fromkeys(txt_url_list))))
+
+    # check_url_available('https://pan.tpym.cn/s/519791006')
