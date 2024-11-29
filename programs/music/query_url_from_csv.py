55f17d6dc485b656c7450c2040d6c9db01e87048
XiaoQi <1091874347@qq.com>
2024-11-30 00:25:21 +0800

    update

--- programs/music/query_url_from_csv.py
+++ programs/music/query_url_from_csv.py
@@ -1,6 +1,22 @@
 from programs.music.music import query_musics_url
 
 
+def _format_df(df):
+    # 提取所需的列并格式化为字符串
+    formatted_strings = []
+    for _, row in df.iterrows():
+        music_name = row['music_name']
+        music_url_baidu = row['music_url_baidu']
+        music_url_kuake = row['music_url_kuake']
+
+        formatted_string = f"{music_name}\n{music_url_baidu}\n{music_url_kuake}\n"
+        formatted_strings.append(formatted_string)
+
+    # 输出结果
+    result = "\n".join(formatted_strings)
+    return result
+
+
 def query_musics(music_names: list):
     s1 = ''
     s2 = ''
@@ -8,7 +24,7 @@ def query_musics(music_names: list):
     existing_names, non_existing_names = query_musics_url(music_names)
 
     if not existing_names.empty:
-        s1 = existing_names.to_string(index=False, header=False)
+        s1 = _format_df(existing_names)
     if len(non_existing_names) > 0:
         s2 = f'没有下面的歌曲：\n{non_existing_names}'
     s = s1 + '\n\n' + s2
