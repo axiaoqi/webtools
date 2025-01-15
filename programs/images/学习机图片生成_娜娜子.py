ce36df2151fa7f19a4f35227a9965dd1af209531
XiaoQi <1091874347@qq.com>
2025-01-15 09:33:44 +0800

    update

---
+++ programs/images/学习机图片生成_娜娜子.py
@@ -0,0 +1,12 @@
+from programs.images.images import run_web_images_susu
+
+if __name__ == '__main__':
+    from programs.settings import SynologyDrive
+    image_dir = SynologyDrive / r'01新项目记录\02_AI学习机\02图片'
+
+    # 主图文件夹
+    primary_images_dir = [image_dir / '素材_娜娜子主图']
+    # 目标文件夹
+    destination = image_dir / '04娜娜子图片'
+
+    run_web_images_susu(primary_images_dir, destination)
