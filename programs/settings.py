a624a47a49c37d231baacafb34b67521b1a6ecf9
XiaoQi <1091874347@qq.com>
2025-03-06 23:15:31 +0800

    update

--- programs/settings.py
+++ programs/settings.py
@@ -3,6 +3,9 @@ from pathlib import Path
 SynologyDrive = Path.home() / 'SynologyDrive'
 if not SynologyDrive.exists():
     SynologyDrive = Path(r'D:\SynologyDrive')
+# mac电脑
+if not SynologyDrive.exists():
+    SynologyDrive = Path('/Volumes/disk3/SynologyDrive')
 
 
 # 学习机文件夹根目录
