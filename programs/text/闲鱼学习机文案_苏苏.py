877c680280dc5097b3e1c0801af619ad4df4e339
XiaoQi <1091874347@qq.com>
2025-06-08 22:28:53 +0800

    update

--- programs/text/闲鱼学习机文案_苏苏.py
+++ programs/text/闲鱼学习机文案_苏苏.py
@@ -12,27 +12,27 @@ class SuSu(XianyuTextGenerator):
         self.choice_items.append(choice_item)
 
         # # 第2句，标题
-        second_list = self._open_file_by_sep(self.data_dir / r'苏苏家_文案\02_标题.txt')
+        second_list = self._open_file_by_sep(self.data_dir / r'智慧学习_文案\02_标题.txt', sep='\n\n')
         choice_item = random.choice(second_list)
         self.choice_items.append(choice_item)
 
         # 第3句，故事，售卖原因--通用文案
-        third_list = self._open_file_by_sep(self.data_dir / r'01通用文案\03_故事.txt')
+        third_list = self._open_file_by_sep(self.data_dir / r'01通用文案\03_故事.txt', sep='\n\n')
         choice_item = random.choice(third_list)
         self.choice_items.append(choice_item)
 
         # 第5句，硬件参数，分割符分割
-        fourth_list = self._open_file_by_sep(self.data_dir / r'苏苏家_文案\05_参数_硬件.txt', sep='\n\n')
+        fourth_list = self._open_file_by_sep(self.data_dir / r'智慧学习_文案\05_参数_硬件.txt', sep='\n\n')
         choice_item = random.choice(fourth_list)
         self.choice_items.append(choice_item)
 
         # 第6句，软件参数，分割符分割
-        fourth_list = self._open_file_by_sep(self.data_dir / r'苏苏家_文案\06_参数_软件.txt', sep='\n\n')
+        fourth_list = self._open_file_by_sep(self.data_dir / r'智慧学习_文案\06_参数_软件.txt', sep='\n\n')
         choice_item = random.choice(fourth_list)
         self.choice_items.append(choice_item)
 
         # 第7句，售后，分割符分割
-        fifth_list = self._open_file_by_sep(self.data_dir / r'苏苏家_文案\09_售后.txt', sep='|||')
+        fifth_list = self._open_file_by_sep(self.data_dir / r'智慧学习_文案\09_售后.txt', sep='\n\n')
         choice_item = random.choice(fifth_list)
         self.choice_items.append(choice_item)
 
